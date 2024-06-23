5e2bec8d8ef5003fe2b35ef68ce3c589c5deb5a2
zEuS0390 <zeusjamesbaltazar@gmail.com>
2024-06-23 15:20:33 +0800

    feature: apply pulse width modulation (PWM) to dim the rgb led light

--- constants.h
+++ constants.h
@@ -1,8 +1,10 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define LED_RED_PIN     2
-#define LED_GREEN_PIN   3
-#define LED_BLUE_PIN    4
+#define NUM_COLORS      3
+#define LED_RED_PIN     3
+#define LED_GREEN_PIN   5
+#define LED_BLUE_PIN    6
+#define DELAY_TIME      10
 
 #endif
