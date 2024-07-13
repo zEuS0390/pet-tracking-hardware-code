d343f903c2c4c15d1a215715fc63873cc5874ea6
Atheo Jester <116144586+AtheoJester23@users.noreply.github.com>
2024-07-13 12:48:03 +0800

    Testing connect internet

--- sketch_jun23a/sketch_jun23a.ino
+++ sketch_jun23a/sketch_jun23a.ino
@@ -1,111 +1,52 @@
 #include <SoftwareSerial.h>
-#include <EEPROM.h>
 
-// Define the pins for the A9G module
-const int RXPin = 4;  // A9G TX pin connected to Arduino RX pin
-const int TXPin = 3;  // A9G RX pin connected to Arduino TX pin
-
-// Create a SoftwareSerial object to communicate with the A9G module
-SoftwareSerial A9GSerial(RXPin, TXPin);
+SoftwareSerial A9G(3, 4); // RX, TX for A9G module
 
 void setup() {
-  // Start serial communication for debugging
-  Serial.begin(115200);
-  Serial.println("Serial started.");
+  Serial.begin(9600);
+  A9G.begin(9600);
+  
+  Serial.println("Starting...");
+  A9G.println("\r");
+  A9G.println("AT\r");
+  delay(10);
 
-  // Initialize software serial communication with the A9G module
-  A9GSerial.begin(115200);
-  Serial.println("SoftwareSerial initialized.");
+  //To connect to the internet
+  A9G.println("AT+CGATT=1\r");
 
-  // Give the module some time to start up
-  delay(2000);
-
-  // Turn on the GPS
-  A9GSerial.println("AT+GPS=1");
+  delay(6000);
+  // Set the PDP context
+  A9G.println("AT+CGDCONT=1,\"IP\",\"smartlte\"\r");
   delay(1000);
+  if (checkResponse("OK")) {
+    Serial.println("A9G has been connected to the internet");
+  } else {
+    Serial.println("Failed to connect A9G to the internet");
+  }
 
-  // Retrieve and print last known location from EEPROM
-  float lastLatitude = readFloatFromEEPROM(0);
-  float lastLongitude = readFloatFromEEPROM(4);
-  Serial.print("Last known Latitude: ");
-  Serial.println(lastLatitude, 6);
-  Serial.print("Last known Longitude: ");
-  Serial.println(lastLongitude, 6);
+  delay(7000);
+  // Initialize HTTP service
+  A9G.println("AT+HTTPINIT\r");
+  delay(7000);
+  if (!checkResponse("OK")) {
+    Serial.println("Failed to initialize HTTP service");
+    return;
+  }
 }
 
 void loop() {
-  // Send the AT+LOCATION=2 command to get GPS location
-  Serial.println("Sending command: AT+LOCATION=2");
-  A9GSerial.println("AT+LOCATION=2");
-  
-  // Allow time for the command to be processed
-  delay(2000); 
+  // Nothing to do in the loop
+}
 
-  // Read the response from the A9G module with a longer timeout
-  unsigned long startTime = millis();
-  String response = "";
-  while (millis() - startTime < 20000) { // 20 seconds timeout
-    while (A9GSerial.available()) {
-      char c = A9GSerial.read();
-      response += c;
-    }
-    if (response.length() > 0) {
-      Serial.println("Response: " + response);
-      break;
+bool checkResponse(const char* response) {
+  long int timeout = millis() + 5000;
+  while (millis() < timeout) {
+    if (A9G.available()) {
+      String reply = A9G.readString();
+      if (reply.indexOf(response) != -1) {
+        return true;
+      }
     }
   }
-
-  // Check if a valid response was received
-  if (response.indexOf("OK") != -1) {
-    // Extract latitude and longitude from the response
-    float latitude = parseLatitude(response);
-    float longitude = parseLongitude(response);
-
-    // Print the latitude and longitude
-    Serial.print("Latitude: ");
-    Serial.println(latitude, 6);
-    Serial.print("Longitude: ");
-    Serial.println(longitude, 6);
-    Serial.println();
-
-    // Store the new location in EEPROM
-    writeFloatToEEPROM(0, latitude);
-    writeFloatToEEPROM(4, longitude);
-  } else {
-    Serial.println("Failed to get a valid GPS location.");
-  }
-
-  // Wait before sending the next command
-  delay(5000); // Adjust delay as needed
-}
-
-float parseLatitude(String response) {
-  int start = response.indexOf(':') + 2;
-  int commaIndex = response.indexOf(',', start);
-  String latString = response.substring(start, commaIndex);
-  return latString.toFloat();
-}
-
-float parseLongitude(String response) {
-  int start = response.indexOf(':') + 2;
-  int firstComma = response.indexOf(',', start);
-  int secondComma = response.indexOf(',', firstComma + 1);
-  String lonString = response.substring(firstComma + 1, secondComma);
-  return lonString.toFloat();
-}
-
-void writeFloatToEEPROM(int address, float value) {
-  byte* bytePointer = (byte*)(void*)&value;
-  for (int i = 0; i < sizeof(float); i++) {
-    EEPROM.write(address + i, bytePointer[i]);
-  }
-}
-
-float readFloatFromEEPROM(int address) {
-  float value = 0.0;
-  byte* bytePointer = (byte*)(void*)&value;
-  for (int i = 0; i < sizeof(float); i++) {
-    bytePointer[i] = EEPROM.read(address + i);
-  }
-  return value;
+  return false;
 }
